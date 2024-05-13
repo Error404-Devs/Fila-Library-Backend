ac02bab968ec3b6c86d741741f66668a21085446
Vlăduț Voicu <voicu.vladutdaniel@yahoo.com>
2024-05-13 11:36:59 +0300

    updated cors origins

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from app.api.endpoints.borrows import borrows_router
 
 app = FastAPI()
 
-origins = ["http://localhost:5173"]
+origins = ["http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
