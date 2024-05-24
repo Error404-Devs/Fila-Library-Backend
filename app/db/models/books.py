c50ba7bcda9a3714f70d57d7c385b10799b3132e
vladono3 <50675017+vladono3@users.noreply.github.com>
2024-05-24 17:59:41 +0300

    Major database update

--- app/db/models/books.py
+++ app/db/models/books.py
@@ -5,8 +5,62 @@ from sqlalchemy.dialects.postgresql import UUID
 from app.db.database import Base
 
 
-class Book(Base):
+class KinderBooks(Base):
     __tablename__ = "books"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
+    title = Column(String, nullable=False)
+    category = Column(String, nullable=True)
+    collection_id = Column(UUID(as_uuid=True), ForeignKey("collections.id"), nullable=True)
+    author_id = Column(UUID(as_uuid=True), ForeignKey("authors.id"), nullable=True)
+    publisher_id = Column(UUID(as_uuid=True), ForeignKey("publishers.id"), nullable=True)
+    year_of_publication = Column(Integer, nullable=True)
+    place_of_publication = Column(String, nullable=True)
+    ISBN = Column(String, nullable=True)
+    price = Column(Integer, nullable=True)
+    UDC = Column(String, nullable=True)
+    created_at = Column(TIMESTAMP, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": str(self.id),
+            "title": str(self.title),
+            "category": str(self.category),
+            "collection_id": str(self.collection_id),
+            "author_id": str(self.author_id),
+            "publisher_id": str(self.publisher_id),
+            "year_of_publication": str(self.year_of_publication),
+            "place_of_publication": str(self.place_of_publication),
+            "ISBN": str(self.ISBN),
+            "price": str(self.price),
+            "UDC": str(self.UDC),
+            "created_at": str(self.created_at)
+        }
+
+    @staticmethod
+    def serialize_books(books):
+        serialized_books = []
+        for book in books:
+            book = {
+                "id": str(book.id),
+                "title": str(book.title),
+                "category": str(book.category),
+                "collection_id": str(book.collection_id),
+                "author_id": str(book.author_id),
+                "publisher_id": str(book.publisher_id),
+                "year_of_publication": str(book.year_of_publication),
+                "place_of_publication": str(book.place_of_publication),
+                "ISBN": str(book.ISBN),
+                "price": str(book.price),
+                "UDC": str(book.UDC),
+                "created_at": str(book.created_at)
+            }
+            serialized_books.append(book)
+        return serialized_books
+
+
+class HighBooks(Base):
+    __tablename__ = "books_high"
 
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
     title = Column(String, nullable=False)
--- app/db/utils/books.py
+++ app/db/utils/books.py
@@ -1,35 +1,35 @@
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import func, String
 
-from app.db.models.books import Book
+from app.db.models.books import KinderBooks, HighBooks
 from app.db.models.authors import Authors
 from app.db.models.publishers import Publishers
 from sqlalchemy import func, String
 from sqlalchemy.exc import SQLAlchemyError
 
 
-def get_books(session, title=None, category=None, publisher=None, author=None, location=None, year=None):
+def get_kinder_books(session, title=None, category=None, publisher=None, author=None, location=None, year=None):
     try:
-        books_query = session.query(Book)
+        books_query = session.query(KinderBooks)
         authors_query = session.query(Authors)
         publishers_query = session.query(Publishers)
 
         # Apply filters if they exist
         if category:
             books_query = books_query.filter(
-                func.lower(func.unaccent(Book.category)).like(f'%{category.lower()}%'))
+                func.lower(func.unaccent(KinderBooks.category)).like(f'%{category.lower()}%'))
 
         if title:
             books_query = books_query.filter(
-                func.lower(func.unaccent(Book.title)).like(f'%{title.lower()}%'))
+                func.lower(func.unaccent(KinderBooks.title)).like(f'%{title.lower()}%'))
 
         if location:
             books_query = books_query.filter(
-                func.lower(func.unaccent(Book.place_of_publication)).like(f'%{location.lower()}%'))
+                func.lower(func.unaccent(KinderBooks.place_of_publication)).like(f'%{location.lower()}%'))
 
         if year:
             books_query = books_query.filter(
-                func.cast(Book.year_of_publication, String).like(f'%{year}%'))
+                func.cast(KinderBooks.year_of_publication, String).like(f'%{year}%'))
 
         if publisher:
             publishers = publishers_query.filter(
@@ -38,7 +38,7 @@ def get_books(session, title=None, category=None, publisher=None, author=None, l
 
             if publishers:
                 publisher_ids = [publisher.id for publisher in publishers]
-                books_query = books_query.filter(Book.publisher_id.in_(publisher_ids))
+                books_query = books_query.filter(KinderBooks.publisher_id.in_(publisher_ids))
             else:
                 return None, "No books found for these filters"
 
@@ -50,14 +50,14 @@ def get_books(session, title=None, category=None, publisher=None, author=None, l
 
             if authors:
                 author_ids = [author.id for author in authors]
-                books_query = books_query.filter(Book.author_id.in_(author_ids))
+                books_query = books_query.filter(KinderBooks.author_id.in_(author_ids))
             else:
                 return None, "No books found for these filters"
 
         books = books_query.all()
 
         if books:
-            return Book.serialize_books(books), None
+            return KinderBooks.serialize_books(books), None
         else:
             return None, "No books found for these filters"
 
@@ -67,32 +67,91 @@ def get_books(session, title=None, category=None, publisher=None, author=None, l
         return None, error
 
 
-def register_book(session,
-                  id,
-                  title,
-                  category,
-                  collection_id,
-                  publisher_id,
-                  author_id,
-                  UDC,
-                  year_of_publication,
-                  place_of_publication,
-                  ISBN,
-                  price,
-                  created_at):
+def get_high_books(session, title=None, category=None, publisher=None, author=None, location=None, year=None):
     try:
-        obj = Book(id=id,
-                   title=title,
-                   category=category,
-                   collection_id=collection_id,
-                   publisher_id=publisher_id,
-                   author_id=author_id,
-                   UDC=UDC,
-                   year_of_publication=year_of_publication,
-                   place_of_publication=place_of_publication,
-                   ISBN=ISBN,
-                   price=price,
-                   created_at=created_at)
+        books_query = session.query(HighBooks)
+        authors_query = session.query(Authors)
+        publishers_query = session.query(Publishers)
+
+        # Apply filters if they exist
+        if category:
+            books_query = books_query.filter(
+                func.lower(func.unaccent(HighBooks.category)).like(f'%{category.lower()}%'))
+
+        if title:
+            books_query = books_query.filter(
+                func.lower(func.unaccent(HighBooks.title)).like(f'%{title.lower()}%'))
+
+        if location:
+            books_query = books_query.filter(
+                func.lower(func.unaccent(HighBooks.place_of_publication)).like(f'%{location.lower()}%'))
+
+        if year:
+            books_query = books_query.filter(
+                func.cast(HighBooks.year_of_publication, String).like(f'%{year}%'))
+
+        if publisher:
+            publishers = publishers_query.filter(
+                func.lower(func.unaccent(Publishers.name)).like(f'%{publisher.lower()}%')
+            ).all()
+
+            if publishers:
+                publisher_ids = [publisher.id for publisher in publishers]
+                books_query = books_query.filter(HighBooks.publisher_id.in_(publisher_ids))
+            else:
+                return None, "No books found for these filters"
+
+        if author:
+            authors = authors_query.filter(
+                func.lower(func.unaccent(Authors.first_name)).like(f'%{author.lower()}%') |
+                func.lower(func.unaccent(Authors.last_name)).like(f'%{author.lower()}%')
+            ).all()
+
+            if authors:
+                author_ids = [author.id for author in authors]
+                books_query = books_query.filter(HighBooks.author_id.in_(author_ids))
+            else:
+                return None, "No books found for these filters"
+
+        books = books_query.all()
+
+        if books:
+            return HighBooks.serialize_books(books), None
+        else:
+            return None, "No books found for these filters"
+
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        print(error)
+        return None, error
+
+
+def register_kinder_book(session,
+                         id,
+                         title,
+                         category,
+                         collection_id,
+                         publisher_id,
+                         author_id,
+                         UDC,
+                         year_of_publication,
+                         place_of_publication,
+                         ISBN,
+                         price,
+                         created_at):
+    try:
+        obj = KinderBooks(id=id,
+                          title=title,
+                          category=category,
+                          collection_id=collection_id,
+                          publisher_id=publisher_id,
+                          author_id=author_id,
+                          UDC=UDC,
+                          year_of_publication=year_of_publication,
+                          place_of_publication=place_of_publication,
+                          ISBN=ISBN,
+                          price=price,
+                          created_at=created_at)
         session.add(obj)
         return obj
     except SQLAlchemyError as e:
@@ -101,20 +160,54 @@ def register_book(session,
         return error
 
 
-def edit_book(session,
-              id,
-              title,
-              category,
-              collection_id,
-              publisher_id,
-              author_id,
-              UDC,
-              year_of_publication,
-              place_of_publication,
-              ISBN,
-              price):
+def register_high_book(session,
+                       id,
+                       title,
+                       category,
+                       collection_id,
+                       publisher_id,
+                       author_id,
+                       UDC,
+                       year_of_publication,
+                       place_of_publication,
+                       ISBN,
+                       price,
+                       created_at):
     try:
-        book = session.query(Book).filter(Book.id == id).first()
+        obj = HighBooks(id=id,
+                        title=title,
+                        category=category,
+                        collection_id=collection_id,
+                        publisher_id=publisher_id,
+                        author_id=author_id,
+                        UDC=UDC,
+                        year_of_publication=year_of_publication,
+                        place_of_publication=place_of_publication,
+                        ISBN=ISBN,
+                        price=price,
+                        created_at=created_at)
+        session.add(obj)
+        return obj
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        print(error)
+        return error
+
+
+def edit_kinder_book(session,
+                     id,
+                     title,
+                     category,
+                     collection_id,
+                     publisher_id,
+                     author_id,
+                     UDC,
+                     year_of_publication,
+                     place_of_publication,
+                     ISBN,
+                     price):
+    try:
+        book = session.query(KinderBooks).filter(KinderBooks.id == id).first()
         if book:
             book.title = title
             book.category = category
@@ -135,13 +228,54 @@ def edit_book(session,
         return None, error
 
 
-def get_book_info(session, book_id):
+def edit_high_book(session,
+                   id,
+                   title,
+                   category,
+                   collection_id,
+                   publisher_id,
+                   author_id,
+                   UDC,
+                   year_of_publication,
+                   place_of_publication,
+                   ISBN,
+                   price):
     try:
-        book = session.query(Book).filter(Book.id == book_id).first()
+        book = session.query(HighBooks).filter(HighBooks.id == id).first()
         if book:
-            return Book.serialize(book), None
+            book.title = title
+            book.category = category
+            book.collection_id = collection_id
+            book.publisher_id = publisher_id
+            book.author_id = author_id
+            book.UDC = UDC
+            book.year_of_publication = year_of_publication
+            book.place_of_publication = place_of_publication
+            book.ISBN = ISBN
+            book.price = price
+            return book.serialize(), None
         else:
-            return None, "No books found"
+            return None, "Book not found"
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        print(error)
+        return None, error
+
+
+def get_book_info(session, book_id, person_location):
+    try:
+        if person_location == "kinder":
+            book = session.query(KinderBooks).filter(KinderBooks.id == book_id).first()
+            if book:
+                return KinderBooks.serialize(book), None
+            else:
+                return None, "No books found"
+        else:
+            book = session.query(HighBooks).filter(HighBooks.id == book_id).first()
+            if book:
+                return HighBooks.serialize(book), None
+            else:
+                return None, "No books found"
     except SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
         print(error)
