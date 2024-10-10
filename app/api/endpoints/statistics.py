bd8547eea58b21b661177277219523ea6400cb13
vladutvoicu <vladut.d.voicu@gmail.com>
2024-10-10 16:01:11 +0300

    added utils and endpoint to export stats as xlsx"

--- app/api/endpoints/statistics.py
+++ app/api/endpoints/statistics.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 statistics_router = APIRouter(tags=["Statistics"])
+from starlette.responses import FileResponse
 
 from app.api.utils.statistics import *
 from app.api.schemas.statistics import *
@@ -12,8 +13,21 @@ auth_handler = AuthHandler()
 @statistics_router.get("/statistics")
 def monthly_statistics(month: int = None, year: int = None, admin_id: str = Depends(auth_handler.auth_wrapper)):
     statistics, error = get_statistics(month, year)
+
     if not error:
         return statistics
     else:
         raise HTTPException(status_code=500, detail=error)
 
+
+@statistics_router.get("/statistics/download")
+def download_monthly_statistics(month: int = None, year: int = None, admin_id: str = Depends(auth_handler.auth_wrapper)):
+    statistics, error = download_statistics(month, year)
+
+    if not error:
+
+        return FileResponse("app/core/files/file.xlsx",
+                            media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                            filename=f"Statistici-{month}-{year}.xlsx")
+    else:
+        raise HTTPException(status_code=500, detail=error)
